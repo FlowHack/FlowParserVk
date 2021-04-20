304dfb3976ac821431bbcf7be6130ac59914441e
Дмитрий <flow.hack.com@gmail.com>
2021-04-20 19:34:36 +0400

    initialize

--- settings/style/fonts.py
+++
@@ -1,8 +0,0 @@
-MAIN_FONT = 'Times New Roman'
-H1_FONT = (MAIN_FONT, 20, 'bold italic')
-H5_FONT = (MAIN_FONT, 14, 'bold italic')
-H6_FONT = (MAIN_FONT, 12, 'bold italic')
-INPUT_FONT = (MAIN_FONT, 10, 'bold italic')
-COMBOBOX_FONT = (MAIN_FONT, 11, 'bold italic')
-SPINBOX_FONT = (MAIN_FONT, 10, 'bold')
-TEXT_FONT = (MAIN_FONT, 12)
