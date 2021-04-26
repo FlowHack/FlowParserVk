d3ef0cb8c757436ad83d04686908cef79c923d87
Дмитрий <flow.hack.com@gmail.com>
2021-04-26 12:59:43 +0400

    release

--- updater.sh
+++ updater.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-echo "Обновление python3 до актуальной версии"
-sleep 2
+echo "!!!!!Обновление python3 до актуальной версии!!!!!"
 sudo apt reinstall python3 -y
 
-echo "Установка python3-venv"
+echo "!!!!!Установка python3-venv!!!!!"
 sleep 2
 sudo apt install python3-venv -y
 
-echo "Установка python3-pip"
+echo "!!!!!Установка python3-pip!!!!!"
 sleep 2
 sudo apt install python3-pip -y
 
-echo "Создание виртуального окружения"
+echo "!!!!!Создание виртуального окружения!!!!!"
 sleep 2
 python3 -m venv venv
 source "$PWD/venv/bin/activate"
 
-echo "Установка зависимостей"
+echo "!!!!!Установка зависимостей!!!!!"
 sleep 2
 pip install wheel
 pip install -r requirements.txt
 
-echo "Запуск программы"
+echo "!!!!!Запуск программы!!!!!"
 python3 linux.py
